79793b2d82e4c9748b5f8f4008468da594065b76
metiks <35481766+metiks@users.noreply.github.com>
2018-01-25 16:50:52 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,5 @@
 function viewArticle(pdf_file, team_names, ppt_link){
-	parent.document.getElementById("pdf_load").src = "https://drive.google.com/file/d/0B6qHZKp28RmeaWNFODR4al9hSTQ/preview";
-	//pdf_file;
+	parent.document.getElementById("pdf_load").src = pdf_file;
 	parent.document.getElementById("team_load").innerHTML = team_names;
 	parent.document.getElementById("ppt_load").innerHTML = ppt_link;
 }
@@ -20,10 +19,9 @@ function loadTeam(kelompok){
 	 	detailTeam = detailTeam + "</ul>";
 	}
 	
-    	file_loc = "materi/" + rubrik + "Kelompok" + kelompok + "-Laporan.pdf";
-   	ppt_loc =  "materi/PPT/" + rubrik + "Kelompok" + kelompok + ".pptx";
-	//file_loc = 'test';
-	//ppt_loc = 'test2';
+    	//file_loc = "materi/" + rubrik + "Kelompok" + kelompok + "-Laporan.pdf";
+   	file_loc = "https://drive.google.com/file/d/0B6qHZKp28RmeaWNFODR4al9hSTQ/preview";
+	ppt_loc =  "materi/PPT/" + rubrik + "Kelompok" + kelompok + ".pptx";
 	ppt_link =  '<a href="'+ppt_loc+'">'+'File Presentasi'+'</a>';	
   	return viewArticle( file_loc, detailTeam, ppt_link);
 }
