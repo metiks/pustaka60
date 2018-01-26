b9387ac281ad32ac8925dc5bd3f265cc12e58568
metiks <35481766+metiks@users.noreply.github.com>
2018-01-26 11:23:19 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -7,23 +7,23 @@ function viewArticle(pdf_file, team_names, ppt_link){
 function loadTeam(kelompok){
 	//window.alert(kelompok);
     for (var i=0; i<=kelompok; i++) {
-        team_rubrik = teamDesc[i][1];
+        	team_rubrik = teamDesc[i][1];
 		team_file = teamDesc[i][2];
 		
-		detailTeam = "<H3>PENULIS</H3>" + "<ul>";
+		team_members = "<H3>PENULIS</H3>" + "<ul>";
 
 	 	jum_team = anggota[kelompok].length;
 
 	 	for (var j=0; j<jum_team; j++) {
-			detailTeam = detailTeam + "<li>" + anggota[kelompok][j] ; 		 	
+			team_members = team_members + "<li>" + anggota[kelompok][j] ; 		 	
 	 	}
-	 	detailTeam = detailTeam + "</ul>";
+	 	team_members = team_members + "</ul>";
 	}
 	
    	file_loc = "https://drive.google.com/file/d/" + team_file + "/preview";
-	ppt_loc =  "materi/PPT/" + rubrik + "Kelompok" + kelompok + ".pptx";
+	ppt_loc =  "materi/PPT/" + team_rubrik + "Kelompok" + kelompok + ".pptx";
 	ppt_link =  '<a href="'+ppt_loc+'">'+'File Presentasi'+'</a>';	
 	window.alert(file_loc);
-  	return viewArticle( file_loc, detailTeam, ppt_link);
+  	return viewArticle( file_loc, team_members, ppt_link);
 }
 
